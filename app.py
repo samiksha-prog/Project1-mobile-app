77b9244de1669c0556c2077abec213661c5d7efc
Samiksha Asati <asatisamiksha13@gmail.com>
2023-05-09 21:24:58 +0530

    final change

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask,request,jsonify
 import numpy as np
 import pickle
-import cv2
-from tensorflow.keras.utils import load_img
-from tensorflow.keras.utils import img_to_array
+from PIL import Image
+from tensorflow.keras.utils import img_to_array, array_to_img
 from tensorflow.keras.models import model_from_json
 from keras.applications.vgg16 import preprocess_input
 
-
 model = pickle.load(open('model.pkl','rb'))
+
 # load json and create model
 json_file = open('vgg_model.json', 'r')
 vgg_model_json = json_file.read()
@@ -26,40 +25,20 @@ def index():
 @app.route('/predict',methods=['POST'])
 def predict():
     f = request.files['file']
-    f.save('input_file.jpg')
-
-    img = cv2.imread('input_file.jpg')
-    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    gray_img = cv2.cvtColor(hsv_img, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray_img, 127, 255, cv2.THRESH_BINARY)
-
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # Set minimum area threshold
-    min_area = 50
-
-    # Iterate through contours
-    for contour in contours:
-        # Calculate area
-        area = cv2.contourArea(contour)
-
-        # Remove contour if area is below threshold
-        if area < min_area:
-            cv2.drawContours(thresh, [contour], 0, 0, -1)
-
-    # Apply mask to original image
-    result = cv2.bitwise_and(img, img, mask=thresh)
-    cv2.imwrite('./test.jpg', result)
+    img = Image.open(f)
+    img_array = img_to_array(img)
 
     def get_features(img_path):
-        img = load_img(img_path, target_size=(224, 224))
-        x = img_to_array(img)
+        img = array_to_img(img_path)
+        resized_img = img.resize((224, 224))
+        x = img_to_array(resized_img)
         x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
         flatten = vgg_model.predict(x)
         return list(flatten[0])
 
     features_t = []
-    features_t.append(get_features('test.jpg'))
+    features_t.append(get_features(img_array))
 
     predicted = model.predict(features_t)[0]
     return jsonify({'status':str(predicted)})
